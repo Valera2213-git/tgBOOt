44fbdaeb4614e33eb29e9df46f78b05ed8967e9e
Валерий <valeramihalev628@gmail.com>
2026-01-18 10:36:53 +0300

    Update data.sql

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,3 +1,13 @@
-INSERT INTO INCOME (ID SERIAL, CHAR_ID, INCOME) VALUES (12345, 2, 3000)
-INSERT INTO ACTIVE_CHAT (ID SERIAL, CHAR_ID) VALUES (12345, 2, 300)
-INSERT INTO SPEND (ID SERIAL, CHAR_ID, SPEND) VALUES (12345, 2, 3000)
+CREATE TABLE IF NOT EXISTS valute_curs (
+id SERIAL PRIMARY KEY,
+name VARCHAR(255),
+nominal INT,
+course DECIMAL(10, 4),
+code VARCHAR(10),
+ch_code VARCHAR(10)
+);
+
+INSERT INTO valute_curs (name, nominal, course, code, ch_code) VALUES
+('Доллар США', 1, 75.0000, 'USD', '840'),
+('Евро', 1, 85,0000, 'EUR', '978'),
+('Китайский юань', 10, 110,0000, 'CNY', '156');
